d7f8d01466dba201f8b1f6a6c68e4ec023a0b55b
Hector del Rio <hector.del.rio@icloud.com>
2015-08-16 01:07:10 +0300

    Bug fixes after real-world test

--- Bootstrap.php
+++ Bootstrap.php
@@ -20,11 +20,11 @@ class Bootstrap implements BootstrapInterface
     /**
      * @var string user table name
      */
-    public $userTableName = 'user';
+    public $userTableName = '{{%user}}';
     /**
-     * @var string|\yii\db\Connection database connection instance or name
+     * @var string database connection instance or name
      */
-    public $db = null;
+    public $db = 'db';
 
     /**
      * @inheritdoc
@@ -35,11 +35,16 @@ class Bootstrap implements BootstrapInterface
             return;
         }
 
+        $db = $app->get($this->db);
+
         $tableNames = Yii::$app->db->getSchema()->tableNames;
         $userModule = $app->getModule('user');
         $adminModule = $app->getModule('admin');
-        $authManager = $app->get('authManager', false);
-        
+        $app->set('authManager', [
+            'class' => 'yii\rbac\DbManager'
+        ]);
+        $authManager = $app->getAuthManager();
+
         if (
             ($userModule !== null and !($userModule instanceof \dektrium\user\Module)) or
             ($adminModule !== null and !($adminModule instanceof \mdm\admin\Module))
@@ -60,38 +65,35 @@ class Bootstrap implements BootstrapInterface
             ]));
         }
 
-        if (!$app->has('user')) {
-            $app->set('user', ArrayHelper::merge($this->admin, [
-                'identityClass' => 'dektrium\user\models\User',
-                'on afterLogin' => function () {
-                    // open session and store user profile in it
-                    $session = Yii::$app->session;
-                    $session->open();
-                    $session->set('profile', Yii::$app->user->identity->getProfile()->one()->attributes);
-                    $session->close();
-                },
-                'on afterLogout' => function () {
-                    // close and destroy session data
-                    Yii::$app->session->destroy();
-                },
-            ]));
-        }
+        $app->set('user', ArrayHelper::merge($this->admin, [
+            'class' => 'yii\web\User',
+            'identityClass' => 'dektrium\user\models\User',
+            'on afterLogin' => function () {
+                // open session and store user profile in it
+                $session = Yii::$app->session;
+                $session->open();
+                $session->set('profile', Yii::$app->user->identity->getProfile()->one()->attributes);
+                $session->close();
+            },
+            'on afterLogout' => function () {
+                // close and destroy session data
+                Yii::$app->session->destroy();
+            },
+        ]));
 
+        $userTableName = $db->schema->getRawTableName($this->userTableName);
+        $authAssignmentTableName = $db->schema->getRawTableName($authManager->assignmentTable);
         // all users with admin role to has full access to yii2-user user management interface
-        if (($authManager instanceof \yii\rbac\DbManager)) {
-            if (!empty(array_diff([$this->userTableName, $authManager->assignmentTable], $tableNames))) {
-                throw new \yii\db\Exception("Unable to find table {$this->userTableName} or {$authManager->assignmentTable}");
-            }
-
-            if (empty(array_diff([$this->userTableName, $authManager->assignmentTable], $tableNames))) {
-                Yii::$app->getModule('user')->admins = (new \yii\db\Query())
-                    ->select('username')
-                    ->from($this->userTableName)
-                    ->leftJoin($authManager->assignmentTable, 'user_id = id')
-                    ->where("item_name LIKE 'admin'")
-                    ->column($this->db);
-            }
+        if (!empty(array_diff([$userTableName, $authAssignmentTableName], $tableNames))) {
+            throw new \yii\db\Exception("Unable to find table {$userTableName} or {$authAssignmentTableName}");
         }
+
+        Yii::$app->getModule('user')->admins = (new \yii\db\Query())
+            ->select('username')
+            ->from($this->userTableName)
+            ->leftJoin($authManager->assignmentTable, 'user_id = id')
+            ->where("item_name LIKE 'admin'")
+            ->column($this->db);
 
         // yii2-admin
         if (empty($adminModule)) {
